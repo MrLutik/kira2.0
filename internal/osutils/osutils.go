f106bdccad2a270fb1412f1c3adc0039aabe4692
PeepoFrog <organizm999@gmail.com>
2023-08-28 11:46:52 +0300

    added check for internet connection for network interfaces

--- internal/osutils/osutils.go
+++ internal/osutils/osutils.go
@@ -1,8 +1,11 @@
 package osutils
 
 import (
+	"net"
 	"os/exec"
+	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/mrlutik/kira2.0/internal/logging"
 )
@@ -30,4 +33,35 @@ func RunCommand(command string, args ...string) ([]byte, error) {
 		return output, err
 	}
 	return output, err
+}
+
+func GetInternetInterface() string {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return ""
+	}
+
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagUp != 0 && iface.Name != "lo" && hasInternetAccess(iface.Name) {
+			return iface.Name
+		}
+	}
+
+	return ""
+}
+
+func hasInternetAccess(ifaceName string) bool {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("ping", "-n", "1", "-w", "2000", "8.8.8.8")
+	case "darwin":
+		cmd = exec.Command("ping", "-c", "1", "-W", "2000", "8.8.8.8")
+	default:
+		cmd = exec.Command("ping", "-c", "1", "-W", "2", "-I", ifaceName, "8.8.8.8")
+	}
+
+	out, err := cmd.CombinedOutput()
+	return err == nil && !strings.Contains(string(out), "100% packet loss")
 }
