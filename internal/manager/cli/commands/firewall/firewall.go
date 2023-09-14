350209aac749375f1ba17e92a8ec016aad1d1311
PeepoFrog <organizm999@gmail.com>
2023-09-14 22:17:29 +0300

    added close-ports open-ports and default flags for firewall

--- internal/manager/cli/commands/firewall/firewall.go
+++ internal/manager/cli/commands/firewall/firewall.go
@@ -1,15 +1,18 @@
 package firewall
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/mrlutik/kira2.0/internal/docker"
 	"github.com/mrlutik/kira2.0/internal/errors"
 	"github.com/mrlutik/kira2.0/internal/firewall/firewallManager"
+	manager "github.com/mrlutik/kira2.0/internal/firewall/firewallManager"
 	"github.com/mrlutik/kira2.0/internal/logging"
 	"github.com/mrlutik/kira2.0/internal/manager/cli/commands/firewall/blacklist"
 	"github.com/mrlutik/kira2.0/internal/manager/cli/commands/firewall/closePort"
 	openport "github.com/mrlutik/kira2.0/internal/manager/cli/commands/firewall/openPort"
 	"github.com/mrlutik/kira2.0/internal/manager/cli/commands/firewall/whitelist"
-	"github.com/mrlutik/kira2.0/internal/osutils"
 	"github.com/spf13/cobra"
 )
 
@@ -42,8 +45,8 @@ func Firewall() *cobra.Command {
 	firewallCmd.AddCommand(blacklist.Blacklist())
 	firewallCmd.AddCommand(whitelist.Whitelist())
 
-	firewallCmd.Flags().Bool("drop-all", false, "Set this flag to block all ports except ssh")
-	firewallCmd.Flags().Bool("allow-all", false, "Set this flag to open all ports")
+	firewallCmd.Flags().Bool("close-ports", false, "Set this flag to block all ports except ssh")
+	firewallCmd.Flags().Bool("open-ports", false, "Set this flag to open all km2 default ports")
 
 	firewallCmd.Flags().Bool("default", false, "Set this flag to restore default setting for firewall (what km2 is set after node installation)")
 
@@ -51,68 +54,59 @@ func Firewall() *cobra.Command {
 }
 
 func validateFlags(cmd *cobra.Command) error {
-	log.Println("validateFlags")
-	// blacklistIP, err := cmd.Flags().GetString("blacklist-ip")
-	// if err != nil {
-	// 	return fmt.Errorf("error retrieving 'ip' flag: %s", err)
-	// }
-	// if blacklistIP != "" {
-	// 	_, err = osutils.CheckIfIPIsValid(blacklistIP)
-	// 	if err != nil {
-	// 		return fmt.Errorf("cannot parse ip: %s", err)
-	// 	}
-	// }
-
-	// whitelistIP, err := cmd.Flags().GetString("whitelist-ip")
-	// if err != nil {
-	// 	return fmt.Errorf("error retrieving 'ip' flag: %s", err)
-	// }
-
-	// if whitelistIP != "" {
-	// 	_, err = osutils.CheckIfIPIsValid(whitelistIP)
-	// 	if err != nil {
-	// 		return fmt.Errorf("cannot parse ip: %s", err)
-	// 	}
-	// }
-
 	return nil
 }
 
 func mainFirewall(cmd *cobra.Command) {
-	blacklistIP, err := cmd.Flags().GetString("blacklist-ip")
-	errors.HandleFatalErr("cannot Get blacklist-ip", err)
 
-	kiraCfg := firewallManager.GenerateKiraConfigForFirewallManager()
+	kiraCfg := manager.GenerateKiraConfigForFirewallManager()
 
-	if blacklistIP != "" {
-		ok, err := osutils.CheckIfIPIsValid(blacklistIP)
-		errors.HandleFatalErr("cannot check if ip is valid", err)
+	log.Println("validating flags")
 
-		if ok {
-			dockerManager, err := docker.NewTestDockerManager()
-			errors.HandleFatalErr("Can't create instance of docker manager", err)
-			defer dockerManager.Cli.Close()
-			fm := firewallManager.NewFirewallManager(dockerManager, kiraCfg)
-			fm.FirewallHandler.BlackListIP(blacklistIP, fm.FirewallConfig.ZoneName)
+	openPorts, err := cmd.Flags().GetBool("open-ports")
+	errors.HandleFatalErr("cannot parse flag", err)
+
+	closePorts, err := cmd.Flags().GetBool("close-ports")
+	errors.HandleFatalErr("cannot parse flag", err)
+
+	defaultB, err := cmd.Flags().GetBool("default")
+	errors.HandleFatalErr("cannot parse flag", err)
+
+	err = validateBoolFlags(openPorts, closePorts, defaultB)
+	errors.HandleFatalErr("only 1 flag can be accepted ", err)
+
+	dockerManager, err := docker.NewTestDockerManager()
+	errors.HandleFatalErr("Can't create instance of docker manager", err)
+	defer dockerManager.Cli.Close()
+
+	fm := firewallManager.NewFirewallManager(dockerManager, kiraCfg)
+	ctx := context.Background()
+
+	switch {
+	case closePorts:
+		err = fm.ClostAllOpenedPorts(ctx)
+		errors.HandleFatalErr("Error while closing ports", err)
+	case openPorts:
+		err = fm.OpenConfigPorts(ctx)
+		errors.HandleFatalErr("Error while opening ports", err)
+	case defaultB:
+		err = fm.SetUpFirewall(ctx)
+		errors.HandleFatalErr("Error while closing ports", err)
+	}
+
+}
+
+// checks if only 1 flag is set to true
+func validateBoolFlags(flags ...bool) error {
+	sum := 0
+	for _, val := range flags {
+		if val {
+			sum++
 		}
 	}
 
-	whitelistIP, err := cmd.Flags().GetString("whitelist-ip")
-	errors.HandleFatalErr("cannot Get whitelist-ip", err)
-
-	if whitelistIP != "" {
-		log.Printf("Checking %s ip is its valid\n", whitelistIP)
-		ok, err := osutils.CheckIfIPIsValid(whitelistIP)
-		errors.HandleFatalErr("cannot check if ip is valid", err)
-
-		if ok {
-			dockerManager, err := docker.NewTestDockerManager()
-			errors.HandleFatalErr("Can't create instance of docker manager", err)
-			defer dockerManager.Cli.Close()
-			fm := firewallManager.NewFirewallManager(dockerManager, kiraCfg)
-			fm.FirewallHandler.WhiteListIp(blacklistIP, fm.FirewallConfig.ZoneName)
-		}
-
+	if sum > 1 {
+		return fmt.Errorf("only one flag at a time is allowed")
 	}
-
+	return nil
 }
