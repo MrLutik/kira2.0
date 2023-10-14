b701271e0ba01bad7ce7d7ded0edb245f0d1cb32
PeepoFrog <organizm999@gmail.com>
2023-10-14 19:06:45 +0300

    working cli

--- internal/manager/cli/commands/maintenance/mainteance.go
+++ internal/manager/cli/commands/maintenance/mainteance.go
@@ -1,7 +1,13 @@
 package maintenance
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/mrlutik/kira2.0/internal/config/configFileController"
+	"github.com/mrlutik/kira2.0/internal/docker"
 	"github.com/mrlutik/kira2.0/internal/logging"
+	"github.com/mrlutik/kira2.0/internal/maintenance"
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +31,11 @@ func Maintenance() *cobra.Command {
 				cmd.Help()
 				return
 			}
-			mainMaitenance(cmd)
+			if err := mainMaitenance(cmd); err != nil {
+				log.Errorf("Errow while executing maintenance command: %s", err)
+				cmd.Help()
+				return
+			}
 		},
 	}
 
@@ -38,9 +48,85 @@ func Maintenance() *cobra.Command {
 
 	return maintenanceCmd
 }
+
 func validateFlags(cmd *cobra.Command) error {
 	return nil
 }
 
-func mainMaitenance(cmd *cobra.Command) {
+func mainMaitenance(cmd *cobra.Command) error {
+	pause, err := cmd.Flags().GetBool("pause")
+	if err != nil {
+		return fmt.Errorf("error while geting <pause> flag")
+	}
+	unpause, err := cmd.Flags().GetBool("unpause")
+	if err != nil {
+		return fmt.Errorf("error while geting <unpause> flag")
+	}
+	activate, err := cmd.Flags().GetBool("activate")
+	if err != nil {
+		return fmt.Errorf("error while geting <activate> flag")
+	}
+	status, err := cmd.Flags().GetBool("status")
+	if err != nil {
+		return fmt.Errorf("error while geting <status> flag")
+	}
+
+	err = validateBoolFlags(cmd, pause, unpause, activate, status)
+	if err != nil {
+		return fmt.Errorf("error, only 1 flag allowed")
+	}
+	log.Debugln(pause, unpause, activate, status)
+	kiraCfg, err := configFileController.ReadOrCreateConfig()
+	if err != nil {
+		return fmt.Errorf("error while geting kira manager config: %s", err)
+	}
+	log.Debugf("kira manager cfg: %+v", kiraCfg)
+	ctx := context.Background()
+	cm, err := docker.NewTestContainerManager()
+	if err != nil {
+		return fmt.Errorf("error while geting containerManager: %s", err)
+	}
+
+	switch {
+	case pause:
+		err = maintenance.PauseValidator(ctx, kiraCfg, cm)
+		if err != nil {
+			return err
+		}
+	case unpause:
+		err = maintenance.UnpauseValidator(ctx, kiraCfg, cm)
+		if err != nil {
+			return err
+		}
+	case activate:
+		err = maintenance.ActivateValidator(ctx, kiraCfg, cm)
+		if err != nil {
+			return err
+		}
+	case status:
+		valStatus, err := maintenance.GetValidatorStatus(ctx, kiraCfg, cm)
+		if err != nil {
+			return err
+		}
+		// log.Infof("Validator Status:\nStatus: %s\nStreak: %s\nRank: %s\n", valStatus.Status, valStatus.Streak, valStatus.Rank)
+		fmt.Printf("***Validator Status***\nStatus: %s\nStreak: %s\nRank: %s\n", valStatus.Status, valStatus.Streak, valStatus.Rank)
+		log.Debugf("valStatus: %+v\n", valStatus)
+	}
+	return nil
+}
+
+func validateBoolFlags(cmd *cobra.Command, flags ...bool) error {
+	sum := 0
+	for _, val := range flags {
+		if val {
+			sum++
+		}
+	}
+
+	if sum > 1 {
+		return fmt.Errorf("only one flag at a time is allowed")
+	} else if sum == 0 {
+		cmd.Help()
+	}
+	return nil
 }
