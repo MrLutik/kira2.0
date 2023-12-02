031da856089b9547f34b8803ac2342c8bb033322
PeepoFrog <organizm999@gmail.com>
2023-12-02 12:37:56 +0200

    restructure

---
+++ internal/gui/tabs/terminal.go
@@ -0,0 +1,25 @@
+package tabs
+
+import (
+	"io"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"github.com/fyne-io/terminal"
+	"golang.org/x/crypto/ssh"
+)
+
+var term = terminal.New()
+
+// var term *terminal.Terminal
+var sshSession *ssh.Session
+var sshIn io.WriteCloser
+var sshOut io.Reader
+
+func makeTerminalScreen(_ fyne.Window) fyne.CanvasObject {
+	go term.RunWithConnection(sshIn, sshOut)
+	return container.NewVScroll(term)
+	// return fyne.NewContainer()
+}
+
+func MakeSSH() {}
