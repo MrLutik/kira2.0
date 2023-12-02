e0b29c772caaaeba221050af566e42f721aee4bc
PeepoFrog <organizm999@gmail.com>
2023-12-02 12:41:07 +0200

    fix

--- internal/gui/tabs/terminal.go
+++ internal/gui/tabs/terminal.go
@@ -12,12 +12,12 @@ import (
 var term = terminal.New()
 
 // var term *terminal.Terminal
-var sshSession *ssh.Session
-var sshIn io.WriteCloser
-var sshOut io.Reader
+var SshSession *ssh.Session
+var SshIn io.WriteCloser
+var SshOut io.Reader
 
 func makeTerminalScreen(_ fyne.Window) fyne.CanvasObject {
-	go term.RunWithConnection(sshIn, sshOut)
+	go term.RunWithConnection(SshIn, SshOut)
 	return container.NewVScroll(term)
 	// return fyne.NewContainer()
 }
