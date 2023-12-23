14f18fd9fd1f2621b0ad1df7860e9fb5b30c7259
PeepoFrog <organizm999@gmail.com>
2023-12-23 15:57:43 +0200

    only start functionality

--- internal/manager/cli/commands/start/start.go
+++ internal/manager/cli/commands/start/start.go
@@ -3,11 +3,9 @@ package start
 import (
 	"context"
 
-	"github.com/mrlutik/kira2.0/internal/adapters"
 	"github.com/mrlutik/kira2.0/internal/config/configFileController"
 	"github.com/mrlutik/kira2.0/internal/docker"
 	"github.com/mrlutik/kira2.0/internal/errors"
-	"github.com/mrlutik/kira2.0/internal/firewall/firewallManager"
 	"github.com/mrlutik/kira2.0/internal/logging"
 	"github.com/mrlutik/kira2.0/internal/manager"
 	"github.com/mrlutik/kira2.0/internal/systemd"
@@ -31,7 +29,6 @@ func Start() *cobra.Command {
 		Long:  long,
 		Run: func(cmd *cobra.Command, _ []string) {
 			recover, _ = cmd.Flags().GetBool("recover")
-
 			mainStart()
 		},
 	}
@@ -39,6 +36,50 @@ func Start() *cobra.Command {
 
 	return startCmd
 }
+
+// func mainStart() {
+// 	systemd.DockerServiceManagement()
+
+// 	dockerManager, err := docker.NewTestDockerManager()
+// 	errors.HandleFatalErr("Can't create instance of docker manager", err)
+// 	defer dockerManager.Cli.Close()
+
+// 	containerManager, err := docker.NewTestContainerManager()
+// 	errors.HandleFatalErr("Can't create instance of container docker manager", err)
+// 	defer containerManager.Cli.Close()
+
+// 	ctx := context.Background()
+
+// 	cfg, err := configFileController.ReadOrCreateConfig()
+// 	errors.HandleFatalErr("Error while reading cfg file", err)
+// 	cfg.Recover = recover
+// 	log.Traceln(recover)
+
+// 	//todo this docker service restart has to be after docker and firewalld instalation, im doin it here because im laucnher is not ready
+// 	err = dockerManager.RestartDockerService()
+// 	errors.HandleFatalErr("Restarting docker service", err)
+// 	docker.VerifyingDockerEnvironment(ctx, dockerManager, cfg)
+// 	// TODO Do we need to safe deb packages in temporary directory?
+// 	// Right now the files are downloaded in current directory, where the program starts
+
+// 	adapters.MustDownloadBinaries(ctx, cfg)
+
+// 	firewallManager := firewallManager.NewFirewallManager(dockerManager, cfg)
+// 	check, err := firewallManager.CheckFirewallSetUp(ctx)
+// 	errors.HandleFatalErr("Error while checking valid firewalld setup", err)
+// 	if !check {
+// 		err = firewallManager.SetUpFirewall(ctx)
+// 		errors.HandleFatalErr("Error while setuping firewall", err)
+// 	}
+
+// 	sekaiManager, err := manager.NewSekaidManager(containerManager, dockerManager, cfg)
+// 	errors.HandleFatalErr("Error creating new 'sekai' manager instance", err)
+// 	sekaiManager.MustInitAndRunGenesisValidator(ctx)
+
+// 	interxManager, err := manager.NewInterxManager(containerManager, cfg)
+// 	errors.HandleFatalErr("Error creating new 'interx' manager instance:", err)
+// 	interxManager.MustInitAndRunInterx(ctx)
+// }
 
 func mainStart() {
 	systemd.DockerServiceManagement()
@@ -59,27 +100,27 @@ func mainStart() {
 	log.Traceln(recover)
 
 	//todo this docker service restart has to be after docker and firewalld instalation, im doin it here because im laucnher is not ready
-	err = dockerManager.RestartDockerService()
-	errors.HandleFatalErr("Restarting docker service", err)
+	// err = dockerManager.RestartDockerService()
+	// errors.HandleFatalErr("Restarting docker service", err)
 	docker.VerifyingDockerEnvironment(ctx, dockerManager, cfg)
 	// TODO Do we need to safe deb packages in temporary directory?
 	// Right now the files are downloaded in current directory, where the program starts
 
-	adapters.MustDownloadBinaries(ctx, cfg)
+	// adapters.MustDownloadBinaries(ctx, cfg)
 
-	firewallManager := firewallManager.NewFirewallManager(dockerManager, cfg)
-	check, err := firewallManager.CheckFirewallSetUp(ctx)
-	errors.HandleFatalErr("Error while checking valid firewalld setup", err)
-	if !check {
-		err = firewallManager.SetUpFirewall(ctx)
-		errors.HandleFatalErr("Error while setuping firewall", err)
-	}
+	// firewallManager := firewallManager.NewFirewallManager(dockerManager, cfg)
+	// check, err := firewallManager.CheckFirewallSetUp(ctx)
+	// errors.HandleFatalErr("Error while checking valid firewalld setup", err)
+	// if !check {
+	// 	err = firewallManager.SetUpFirewall(ctx)
+	// 	errors.HandleFatalErr("Error while setuping firewall", err)
+	// }
 
 	sekaiManager, err := manager.NewSekaidManager(containerManager, dockerManager, cfg)
 	errors.HandleFatalErr("Error creating new 'sekai' manager instance", err)
-	sekaiManager.MustInitAndRunGenesisValidator(ctx)
+	sekaiManager.MustRunSekaid(ctx)
 
 	interxManager, err := manager.NewInterxManager(containerManager, cfg)
 	errors.HandleFatalErr("Error creating new 'interx' manager instance:", err)
-	interxManager.MustInitAndRunInterx(ctx)
+	interxManager.MustRunInterx(ctx)
 }
