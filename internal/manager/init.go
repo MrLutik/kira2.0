254b7e7e8b432a673125fea8b372b16521eff08d
PeepoFrog <organizm999@gmail.com>
2023-12-23 17:57:46 +0200

    only init for interx

--- internal/manager/init.go
+++ internal/manager/init.go
@@ -91,14 +91,20 @@ func (i *InterxManager) MustInitAndRunInterx(ctx context.Context) {
 
 //run_stop_init branch
 
+// initilazing new node which connects to existing network
+// It delegates the setup process to mustInitializeAndRunContainer method with 'isGenesisValidator' - false and with provided genesis file.
 func (s *SekaidManager) MustInitJoiner(ctx context.Context, genesis []byte) {
-	s.mustInitializeJoiner(ctx, genesis, false)
+	s.mustInitializeContainer(ctx, genesis, false)
+}
+
+func (s *SekaidManager) MustInitNew(ctx context.Context) {
+	s.mustInitializeContainer(ctx, nil, true)
 }
 
 // mustInitializeJoiner sets up containers based on the provided configuration and runs the Sekaid container.
 // If the 'isGenesisValidator' flag is set to true, it sets up the container for the genesis validator, otherwise for the joiner.
 // The method will terminate the program with a fatal error if any step encounters an error.
-func (s *SekaidManager) mustInitializeJoiner(ctx context.Context, genesis []byte, isGenesisValidator bool) {
+func (s *SekaidManager) mustInitializeContainer(ctx context.Context, genesis []byte, isGenesisValidator bool) {
 	usr := osutils.GetCurrentOSUser()
 	s.config.SecretsFolder = usr.HomeDir + "/.secrets"
 
@@ -128,6 +134,32 @@ func (s *SekaidManager) mustInitializeJoiner(ctx context.Context, genesis []byte
 	err = s.containerManager.InstallDebPackage(ctx, s.config.SekaidContainerName, debFileDestInContainer+s.config.SekaiDebFileName)
 	errors.HandleFatalErr("Installing dep package in container", err)
 
-	err = s.initJoinerSekaidBinInContainer(ctx, genesis)
+	if isGenesisValidator {
+		err = s.initGenesisSekaidBinInContainer(ctx)
+	} else {
+		// err = s.runJoinerSekaidContainer(ctx, genesis)
+		err = s.initJoinerSekaidBinInContainer(ctx, genesis)
+	}
 	errors.HandleFatalErr("Setup container", err)
+}
+
+// MustInitAndRunInterx initializes and runs the Interx container.
+// The method will terminate the program with a fatal error if any step encounters an error.
+func (i *InterxManager) MustInitInterx(ctx context.Context) {
+	check, err := i.containerManager.CheckForContainersName(ctx, i.config.InterxContainerName)
+	errors.HandleFatalErr("Checking container names", err)
+	if check {
+		i.containerManager.StopAndDeleteContainer(ctx, i.config.InterxContainerName)
+		errors.HandleFatalErr("Deleting container", err)
+	}
+
+	errors.HandleFatalErr("Interx managing", err)
+	err = i.containerManager.InitAndCreateContainer(ctx, i.ContainerConfig, i.InterxNetworkConfig, i.InterxHostConfig, i.config.InterxContainerName)
+	errors.HandleFatalErr("Interx initialization", err)
+
+	err = i.containerManager.SendFileToContainer(ctx, i.config.InterxDebFileName, debFileDestInContainer, i.config.InterxContainerName)
+	errors.HandleFatalErr("Sending file to container", err)
+
+	err = i.containerManager.InstallDebPackage(ctx, i.config.InterxContainerName, debFileDestInContainer+i.config.InterxDebFileName)
+	errors.HandleFatalErr("Installing dep package in container", err)
 }
