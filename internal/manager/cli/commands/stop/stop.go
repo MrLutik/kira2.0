7413ba47dee3fe8cfab61da5fc3bff81200ea8e1
PeepoFrog <organizm999@gmail.com>
2023-12-26 15:08:59 +0200

    stop cmd

--- internal/manager/cli/commands/stop/stop.go
+++ internal/manager/cli/commands/stop/stop.go
@@ -1,1 +1,60 @@
 package stop
+
+import (
+	"context"
+
+	"github.com/mrlutik/kira2.0/internal/config/configFileController"
+	"github.com/mrlutik/kira2.0/internal/docker"
+	"github.com/mrlutik/kira2.0/internal/errors"
+	"github.com/mrlutik/kira2.0/internal/logging"
+	"github.com/mrlutik/kira2.0/internal/manager"
+	"github.com/mrlutik/kira2.0/internal/systemd"
+	"github.com/spf13/cobra"
+)
+
+const (
+	use   = "stop"
+	short = "Stop kira node"
+	long  = "Stop node if running"
+)
+
+var log = logging.Log
+
+func Stop() *cobra.Command {
+	log.Info("Adding `start` command...")
+	startCmd := &cobra.Command{
+		Use:   use,
+		Short: short,
+		Long:  long,
+		Run: func(cmd *cobra.Command, _ []string) {
+			mainStop(cmd)
+		},
+	}
+
+	return startCmd
+}
+
+func mainStop(cmd *cobra.Command) {
+	systemd.DockerServiceManagement()
+
+	dockerManager, err := docker.NewTestDockerManager()
+	errors.HandleFatalErr("Can't create instance of docker manager", err)
+	defer dockerManager.Cli.Close()
+
+	containerManager, err := docker.NewTestContainerManager()
+	errors.HandleFatalErr("Can't create instance of container docker manager", err)
+	defer containerManager.Cli.Close()
+
+	ctx := context.Background()
+
+	cfg, err := configFileController.ReadOrCreateConfig()
+	errors.HandleFatalErr("Error while reading cfg file", err)
+	docker.VerifyingDockerEnvironment(ctx, dockerManager, cfg)
+	sekaiManager, err := manager.NewSekaidManager(containerManager, dockerManager, cfg)
+	errors.HandleFatalErr("Error creating new 'sekai' manager instance", err)
+	sekaiManager.MustStopSekaid(ctx)
+
+	interxManager, err := manager.NewInterxManager(containerManager, cfg)
+	errors.HandleFatalErr("Error creating new 'interx' manager instance:", err)
+	interxManager.MustStopInterxd(ctx)
+}
---
+++ internal/manager/stop.go
@@ -0,0 +1,40 @@
+package manager
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/mrlutik/kira2.0/internal/errors"
+	"github.com/mrlutik/kira2.0/internal/logging"
+)
+
+var log = logging.Log
+
+func (s *SekaidManager) MustStopSekaid(ctx context.Context) {
+	p := "sekaid"
+	code := "15"
+	log.Printf("Stoping <%s> proccess\n", p)
+	out, err := s.containerManager.ExecCommandInContainer(ctx, s.config.SekaidContainerName, []string{"pkill", "-" + code, p})
+	if err != nil {
+		errors.HandleFatalErr(fmt.Sprintf("cannot kill <%s> process inside  <%s> container\nout: %s", p, s.config.SekaidContainerName, string(out)), err)
+	}
+	log.Printf("<%s> proccess was successfully stoped\n", p)
+
+	err = s.dockerManager.Cli.ContainerStop(ctx, s.config.SekaidContainerName, container.StopOptions{})
+	errors.HandleFatalErr("cannot stop container", err)
+}
+
+func (i *InterxManager) MustStopInterxd(ctx context.Context) {
+	p := "interxd"
+	code := "9"
+	log.Printf("Stoping <%s> proccess\n", p)
+	out, err := i.containerManager.ExecCommandInContainer(ctx, i.config.InterxContainerName, []string{"pkill", "-" + code, p})
+	if err != nil {
+		errors.HandleFatalErr(fmt.Sprintf("cannot kill <%s> process inside <%s> container\nout: %s", p, i.config.InterxContainerName, string(out)), err)
+	}
+	log.Printf("<%s> proccess was successfully stoped\n", p)
+
+	err = i.containerManager.Cli.ContainerStop(ctx, i.config.InterxContainerName, container.StopOptions{})
+	errors.HandleFatalErr("cannot stop container", err)
+}
