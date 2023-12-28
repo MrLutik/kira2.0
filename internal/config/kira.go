587782c8f5cbfc4d4c9c4d35eaaef8976b1c23a2
PeepoFrog <organizm999@gmail.com>
2023-12-28 12:33:29 +0200

    name channing + method of getting a volumeMountPath

--- internal/config/kira.go
+++ internal/config/kira.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"time"
 
 	vlg "github.com/PeepoFrog/validator-key-gen/MnemonicsGenerator"
@@ -33,7 +34,7 @@ type KiraConfig struct {
 	SekaidContainerName string // name for sekai container
 	InterxContainerName string // name for interx container
 	VolumeName          string // the name of a docker's volume that will be SekaidContainerName and InterxContainerName will be using
-	DataPathInVolume    string
+	VolumeMoutPath      string
 	MnemonicDir         string                 // destination where mnemonics file will be saved
 	RpcPort             string                 // sekaid's rpc port
 	GrpcPort            string                 // sekaid's grpc port
@@ -52,4 +53,8 @@ type KiraConfig struct {
 	// NOTE Default time of block is ~5 seconds!
 	// Check (m *MonitoringService) GetConsensusInfo method
 	// from cmd/monitoring/main.go
+}
+
+func (cfg *KiraConfig) GetVolumeMountPoint() string {
+	return fmt.Sprintf("%s:%s", cfg.VolumeName, cfg.VolumeMoutPath)
 }
