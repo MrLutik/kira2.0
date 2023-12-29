3c3d96c70f9c4ce5c11b9ef1fb1aeb37a5e042a0
PeepoFrog <organizm999@gmail.com>
2023-12-29 20:38:06 +0200

    unified and more robust func for stoping process in container

--- internal/manager/stop.go
+++ internal/manager/stop.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/docker/docker/api/types/container"
+	"github.com/mrlutik/kira2.0/internal/docker"
 	"github.com/mrlutik/kira2.0/internal/errors"
 	"github.com/mrlutik/kira2.0/internal/logging"
 )
@@ -12,29 +13,46 @@ import (
 var log = logging.Log
 
 func (s *SekaidManager) MustStopSekaid(ctx context.Context) {
-	p := "sekaid"
-	code := "15"
-	log.Printf("Stoping <%s> proccess\n", p)
-	out, err := s.containerManager.ExecCommandInContainer(ctx, s.config.SekaidContainerName, []string{"pkill", "-" + code, p})
-	if err != nil {
-		errors.HandleFatalErr(fmt.Sprintf("cannot kill <%s> process inside  <%s> container\nout: %s", p, s.config.SekaidContainerName, string(out)), err)
-	}
-	log.Printf("<%s> proccess was successfully stoped\n", p)
+	err := stopProcessInsideContainer(ctx, "sekaid", 15, s.config.SekaidContainerName, s.containerManager)
+	errors.HandleFatalErr("Stoping sekaid bin in container", err)
 	log.Printf("Stoping <%s> container\n", s.config.SekaidContainerName)
 	err = s.dockerManager.Cli.ContainerStop(ctx, s.config.SekaidContainerName, container.StopOptions{})
-	errors.HandleFatalErr("cannot stop container", err)
+	errors.HandleFatalErr(fmt.Sprintf("cannot stop %s container", s.config.SekaidContainerName), err)
 }
 
 func (i *InterxManager) MustStopInterx(ctx context.Context) {
-	p := "interx"
-	code := "9"
-	log.Printf("Stoping <%s> proccess\n", p)
-	out, err := i.containerManager.ExecCommandInContainer(ctx, i.config.InterxContainerName, []string{"pkill", "-" + code, p})
-	if err != nil {
-		errors.HandleFatalErr(fmt.Sprintf("cannot kill <%s> process inside <%s> container\nout: %s", p, i.config.InterxContainerName, string(out)), err)
-	}
-	log.Printf("<%s> proccess was successfully stoped\n", p)
+	err := stopProcessInsideContainer(ctx, interxProcessName, 9, i.config.InterxContainerName, i.containerManager)
+	errors.HandleFatalErr("Stoping interx bin in container", err)
 	log.Printf("Stoping <%s> container\n", i.config.InterxContainerName)
 	err = i.containerManager.Cli.ContainerStop(ctx, i.config.InterxContainerName, container.StopOptions{})
-	errors.HandleFatalErr("cannot stop container", err)
+	errors.HandleFatalErr(fmt.Sprintf("cannot stop %s container", i.config.InterxContainerName), err)
+}
+
+func stopProcessInsideContainer(ctx context.Context, processName string, codeTopStopWIth int, containerName string, containerManager *docker.ContainerManager) error {
+	log.Printf("Checking if %s is running inside container", processName)
+	check, _, err := containerManager.CheckIfProcessIsRunningInContainer(ctx, processName, containerName)
+	if err != nil {
+		return fmt.Errorf("cant check if procces is running inside container, %s", err)
+	}
+	if !check {
+		log.Warnf("process <%s> is not running inside <%s> container\n", processName, containerName)
+		return nil
+	}
+	log.Printf("Stoping <%s> proccess\n", processName)
+	out, err := containerManager.ExecCommandInContainer(ctx, containerName, []string{"pkill", fmt.Sprintf("-%v", codeTopStopWIth), processName})
+	if err != nil {
+		log.Errorf("cannot kill <%s> process inside <%s> container\nout: %s\nerr: %v\n", processName, containerName, string(out), err)
+		return fmt.Errorf("cannot kill <%s> process inside <%s> container\nout: %s\nerr: %s", processName, containerName, string(out), err)
+	}
+
+	check, _, err = containerManager.CheckIfProcessIsRunningInContainer(ctx, processName, containerName)
+	if err != nil {
+		return fmt.Errorf("cant check if procces is running inside container, %s", err)
+	}
+	if check {
+		log.Errorf("Process <%s> is still running inside <%s> container\n", processName, containerName)
+		return err
+	}
+	log.Printf("<%s> proccess was successfully stoped\n", processName)
+	return nil
 }
