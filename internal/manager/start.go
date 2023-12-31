5b74b4919dad96293449077f49f4449bdfdd82dc
PeepoFrog <organizm999@gmail.com>
2023-12-31 19:44:24 +0200

    docs and err handling for container start

--- internal/manager/start.go
+++ internal/manager/start.go
@@ -7,17 +7,19 @@ import (
 	"github.com/mrlutik/kira2.0/internal/errors"
 )
 
+// MustRunSekaid if exist starts sekaid container and running sekaid bin inside
 func (s *SekaidManager) MustRunSekaid(ctx context.Context) {
-	s.containerManager.StartContainer(ctx, s.config.SekaidContainerName)
-	err := s.startSekaidBinInContainer(ctx)
+	err := s.containerManager.StartContainer(ctx, s.config.SekaidContainerName)
+	errors.HandleFatalErr(fmt.Sprintf("Cannot start '%s' container", s.config.SekaidContainerName), err)
+	err = s.startSekaidBinInContainer(ctx)
 	errors.HandleFatalErr(fmt.Sprintf("Cannot start 'sekaid' bin in '%s' container", s.config.SekaidContainerName), err)
-
-	// log.Errorf("Cannot start 'sekaid' bin in '%s' container, error: %s", s.config.SekaidContainerName, err)
 }
 
+// MustRunInterx if exist starts interx container and running interx bin inside
 func (i *InterxManager) MustRunInterx(ctx context.Context) {
-	i.containerManager.StartContainer(ctx, i.config.InterxContainerName)
-	err := i.startInterxBinInContainer(ctx)
+	err := i.containerManager.StartContainer(ctx, i.config.InterxContainerName)
+	errors.HandleFatalErr(fmt.Sprintf("Cannot start '%s' container", i.config.InterxContainerName), err)
+	err = i.startInterxBinInContainer(ctx)
 	errors.HandleFatalErr(fmt.Sprintf("Cannot start 'interx' bin in '%s' container", i.config.InterxContainerName), err)
 
 }
