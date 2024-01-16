d09a4243d822f9c33fe4a8fa3cb46fa949fca84d
Andrii Derkach <andrsj.derkach@gmail.com>
2024-01-17 01:06:00 +0200

    fixing internal/monitoring/interx.go

--- internal/monitoring/errors.go
+++ internal/monitoring/errors.go
@@ -2,10 +2,20 @@ package monitoring
 
 import "fmt"
 
-type HTTPRequestFailedError struct {
-	StatusCode int
-}
+type (
+	HTTPRequestFailedError struct {
+		StatusCode int
+	}
+
+	ValidatorAddressNotFoundError struct {
+		Address string
+	}
+)
 
 func (e *HTTPRequestFailedError) Error() string {
 	return fmt.Sprintf("HTTP request failed with status: %d", e.StatusCode)
+}
+
+func (e *ValidatorAddressNotFoundError) Error() string {
+	return fmt.Sprintf("can't find validator address '%s'", e.Address)
 }
