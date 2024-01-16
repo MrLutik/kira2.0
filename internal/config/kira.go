542671415bf486724c5aaa41727d1f37160df329
Andrii Derkach <andrsj.derkach@gmail.com>
2024-01-16 02:29:51 +0200

    fixing internal/config/kira.go

--- internal/config/kira.go
+++ internal/config/kira.go
@@ -17,39 +17,39 @@ type (
 
 	// JsonValue represents a configuration value to be updated in the '*.json' file of the 'interx' application
 	JsonValue struct {
-		Key   string // dot-separated keys by nesting
+		Key   string // Dot-separated keys by nesting
 		Value any
 	}
 
 	// KiraConfig is a configuration for sekaid or interx manager.
 	KiraConfig struct {
-		NetworkName         string                 // name of a blockchain name (chain-ID)
-		SekaidHome          string                 // home folder for sekai bin
-		InterxHome          string                 // home folder for interx bin
-		KeyringBackend      string                 // name of keyring backend
-		DockerImageName     string                 // name of a docker image that will be used to create containers for sekai and interx
-		DockerImageVersion  string                 // version of a docker image that will be used to create containers for sekai and interx
-		DockerNetworkName   string                 // the name of docker network that will be create and used for sekaid and interx containers
-		SekaiVersion        string                 // version of sekai binary
-		InterxVersion       string                 // version of interx binary
-		SekaidContainerName string                 // name for sekai container
-		InterxContainerName string                 // name for interx container
-		VolumeName          string                 // the name of a docker's volume that will be SekaidContainerName and InterxContainerName will be using
-		VolumeMoutPath      string                 // mount point in docker volume for containers
-		MnemonicDir         string                 // destination where mnemonics file will be saved
-		RpcPort             string                 // sekaid's rpc port
-		GrpcPort            string                 // sekaid's grpc port
-		P2PPort             string                 // sekaid's p2p port
-		PrometheusPort      string                 // prometheus port
-		InterxPort          string                 // interx endpoint port
+		NetworkName         string                 // Name of a blockchain name (chain-ID)
+		SekaidHome          string                 // Home folder for sekai bin
+		InterxHome          string                 // Home folder for interx bin
+		KeyringBackend      string                 // Name of keyring backend
+		DockerImageName     string                 // Name of a docker image that will be used to create containers for sekai and interx
+		DockerImageVersion  string                 // Version of a docker image that will be used to create containers for sekai and interx
+		DockerNetworkName   string                 // The name of docker network that will be create and used for sekaid and interx containers
+		SekaiVersion        string                 // Version of sekai binary
+		InterxVersion       string                 // Version of interx binary
+		SekaidContainerName string                 // Name for sekai container
+		InterxContainerName string                 // Name for interx container
+		VolumeName          string                 // The name of a docker's volume that will be SekaidContainerName and InterxContainerName will be using
+		VolumeMoutPath      string                 // Mount point in docker volume for containers
+		MnemonicDir         string                 // Destination where mnemonics file will be saved
+		RpcPort             string                 // Sekaid's rpc port
+		GrpcPort            string                 // Sekaid's grpc port
+		P2PPort             string                 // Sekaid's p2p port
+		PrometheusPort      string                 // Prometheus port
+		InterxPort          string                 // Interx endpoint port
 		Moniker             string                 // Moniker
-		SekaiDebFileName    string                 // fileName of sekai deb file
-		InterxDebFileName   string                 // fileName of interx deb file
-		SecretsFolder       string                 // path where mnemonics.env and node keys located
+		SekaiDebFileName    string                 // File name of sekai deb file
+		InterxDebFileName   string                 // File name of interx deb file
+		SecretsFolder       string                 // Path to mnemonics.env and node keys
 		TimeBetweenBlocks   time.Duration          // Awaiting time between blocks
-		KiraConfigFilePath  string                 // string to toml km2 config file //default /home/$USER/.config/kira2/kiraConfig.toml
-		ConfigTomlValues    []TomlValue            //`toml:"-"` // List of configs for update
-		Recover             bool                   `toml:"-"` // switch for recover mode
+		KiraConfigFilePath  string                 // Path to toml km2 config file //default /home/$USER/.config/kira2/kiraConfig.toml
+		ConfigTomlValues    []TomlValue            `toml:"-"` // List of configs for update
+		Recover             bool                   `toml:"-"` // Switch for recover mode
 		MasterMnamonicSet   *vlg.MasterMnemonicSet `toml:"-"`
 		// NOTE Default time of block is ~5 seconds!
 		// Check (m *MonitoringService) GetConsensusInfo method
